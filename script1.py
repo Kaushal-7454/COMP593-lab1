2dfe2be71226a2f62407e0773f3adf91552eb10e
Daniel Palij <danielpalij@gmail.com>
2023-05-17 15:27:11 -0400

    modified both scripts
    deleted from script1.py
    -added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #print both lines from Zen of Python
 print("Although that way may not be obious at first unless you're Dutch")
-print("Complex is better than complicated.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+#print both lines from Zen of Python
 print("Readability counts.")
 print("There should be one-- and preferable only one --obvious way to do it.")
