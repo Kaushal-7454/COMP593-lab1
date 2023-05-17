3a44dd74fdd87310238d9b236d6a82316edc53dc
Daniel Palij <danielpalij@gmail.com>
2023-05-17 15:21:02 -0400

    added two lines to both scripts

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 #Print lines from Zen of Python
 print("Although that way may not be obious at first unless you're Dutch")
+print("Complex is better than complicated.")
--- script2.py
+++ script2.py
@@ -1,1 +1,2 @@
+print("Readability counts.")
 print("There should be one-- and preferable only one --obvious way to do it.")
