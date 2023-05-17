3ac0c78401716ad0727f082b28e739eb219d09b5
Daniel Palij <KaushalkumarMaheshkumar.Limbachiya@flemingcollege.ca>
2023-05-17 15:59:15 -0400

    modified both scripts
    -deleted a print() statement from script1.py
    Added two print statement to script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #print both lines from Zen of Python
 print("Readability counts.")
 print("There should be one-- and preferable only one --obvious way to do it.")
+print("printing one more print statement to this script")
